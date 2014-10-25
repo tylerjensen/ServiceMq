437320e9757b1f080931e80dffb96374d98f32db
Tyler Jensen <tyler.jensen@gmail.com>
2014-10-25 11:27:28 -0600

    v1.2.0 release adds Broadcast messaging

--- src/ServiceMq/ServiceMq/Properties/AssemblyInfo.cs
+++ src/ServiceMq/ServiceMq/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ServiceMq")]
 [assembly: AssemblyDescription("A store and forward message queue for .NET.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Duovia Inc.")]
+[assembly: AssemblyCompany("Tyler Jensen")]
 [assembly: AssemblyProduct("ServiceMq")]
 [assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
